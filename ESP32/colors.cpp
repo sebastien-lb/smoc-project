ee67b332d78fede3332820a1eb594871bce59bc0
Clement Dessoude <clement.dessoude@gmail.com>
2019-01-23 21:58:41 +0100

    fix(all): fix pointer issues, switch cases and memory allocation

--- ESP32/colors.cpp
+++ ESP32/colors.cpp
@@ -4,16 +4,16 @@
 uint32_t getColorConfig(Adafruit_NeoPixel *pixels, int colorCode){
     switch (colorCode) {
         case RED:
-            return pixels.Color(10, 0, 0);
+            return pixels->Color(10, 0, 0);
         case BLUE:
-            return pixels.Color(0, 0, 10);
+            return pixels->Color(0, 0, 10);
         case GREEN:
-            return pixels.Color(0, 10, 0);
+            return pixels->Color(0, 10, 0);
         case YELLOW:
-            return pixels.Color(10, 10, 0);
+            return pixels->Color(10, 10, 0);
         case MAGENTA:
-            return pixels.Color(10, 0, 10);
+            return pixels->Color(10, 0, 10);
         case CYAN:
-            return pixels.Color(0, 10, 10);
+            return pixels->Color(0, 10, 10);
     }
 }
